df69367479801c5a91c28f2292de8049173ee7f6
mengbinggg <1247779079@qq.com>
2020-03-27 17:17:02 +0800

    添加day02项目

---
+++ day02/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+ss
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
